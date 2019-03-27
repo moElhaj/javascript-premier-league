4cb0489b5e6e09559445baafab29e375f5cbf34b
Mohamed Elhaj <mo.elhaj@outlook.com>
2019-03-27 22:41:39 +0200

    Update date.js

--- src/components/date.js
+++ src/components/date.js
@@ -1,39 +1,37 @@
-export function dateToday() { 
+export function isToday(date) {
     let today = new Date();
     let day = today.getDate();
     day = day < 10 ? `0${day}` : day;
     let month = today.getMonth() + 1;
     month = month < 10 ? `0${month}` : month;
     let year = today.getFullYear();
-
-    return `${year}-${month}-${day}`;
+    let todaysDate = `${year}-${month}-${day}`;
+    if (date == todaysDate) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-export function dateTomorrow(){
+export function isTomorrow(date) {
     let today = new Date();
     today.setDate(today.getDate() + 1);
-    
+
     let day = today.getDate();
     day = day < 10 ? `0${day}` : day;
     let month = today.getMonth() + 1;
     month = month < 10 ? `0${month}` : month;
     let year = today.getFullYear();
-    
-    return `${year}-${month}-${day}`;
+    let tomorrowsDate = `${year}-${month}-${day}`;
+    if (date == tomorrowsDate) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-
-export function dateWeek() {
-    let weeksDates = [];
-    let today = new Date();
-    for (let i = 0; i < 7; i++) {
-        today.setDate(today.getDate() + 1);
-        let day = today.getDate();
-        day = day < 10 ? `0${day}` : day;
-        let month = today.getMonth() + 1;
-        month = month < 10 ? `0${month}` : month;
-        let year = today.getFullYear();
-        weeksDates.push(`${day}-${month}-${year}`)
-    }   
-    return weeksDates; 
+export function matchTime(utcDate){
+    let houre = utcDate.slice(11, 13);
+    let minutes = utcDate.slice(14, 16);
+    return `${houre}:${minutes}`;
 }
